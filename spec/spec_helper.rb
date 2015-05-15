e997baaa9af1189ed29e3d9194df35cb798c0026
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-05-16 00:54:53 +0800

    disable rspec monkey patching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'zero_downtime'
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+end
--- spec/zero_downtime_spec.rb
+++ spec/zero_downtime_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ZeroDowntime do
+RSpec.describe ZeroDowntime do
   it 'has a version number' do
     expect(ZeroDowntime::VERSION).not_to be nil
   end
