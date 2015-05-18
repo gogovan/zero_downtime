97fcc1bf64173726dd8b269053f9702c24d424d4
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-05-18 16:49:39 +0800

    make sure the railtie is loadable

--- lib/zero_downtime.rb
+++ lib/zero_downtime.rb
@@ -1,5 +1,6 @@
 require 'zero_downtime/version'
 require 'zero_downtime/deprecatable'
+require 'zero_downtime/railtie' if defined?(Rails)
 
 # ZeroDowntime is a simple library for enabling zero-downtime
 # with active record classes
